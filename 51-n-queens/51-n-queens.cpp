18d5d52038794ab58e44812a241a6bf21acf8880
Manish Sencha <39882835+manishsencha@users.noreply.github.com>
2022-08-14 15:21:52 +0530

    Time: 14 ms (41.45%), Space: 7.2 MB (70.72%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n, '.');
+        for(int i = 0; i < n; ++i) {
+            board[i] = s;
+        }
+        func(0, board, ans, n);
+        return ans;
+    }
+    void func(int col, vector<string> &board, vector<vector<string>> &ans, int n) {
+        if(col == n) {
+            ans.push_back(board);
+            return;
+        }
+        for(int row = 0; row < n; row++) {
+            if(isSafe(row, col, board, n)) {
+                board[row][col] = 'Q';
+                func(col + 1, board, ans, n);
+                board[row][col] = '.';    
+            }
+        }
+    }
+    bool isSafe(int row, int col, vector<string> &board, int n) {
+        for(int i = row, j = col; i >= 0 && j >= 0; --i, --j) {
+            if(board[i][j] == 'Q') return false;
+        }
+        for(int i = col; i>= 0; --i) {
+            if(board[row][i] == 'Q') return false;
+        }
+        for(int i = row, j = col; i < n && j >= 0; ++i, --j) {
+             if(board[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+};
