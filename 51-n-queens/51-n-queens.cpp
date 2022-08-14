153fd1428b09336dcd8fba39a12d104931d4e692
Manish Sencha <39882835+manishsencha@users.noreply.github.com>
2022-08-14 15:23:55 +0530

    Time: 3 ms (96.64%), Space: 7.2 MB (70.72%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -24,13 +24,13 @@ public:
         }
     }
     bool isSafe(int row, int col, vector<string> &board, int n) {
-        for(int i = row, j = col; i >= 0 && j >= 0; --i, --j) {
+        for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j) {
             if(board[i][j] == 'Q') return false;
         }
-        for(int i = col; i>= 0; --i) {
+        for(int i = col - 1; i >= 0; --i) {
             if(board[row][i] == 'Q') return false;
         }
-        for(int i = row, j = col; i < n && j >= 0; ++i, --j) {
+        for(int i = row + 1, j = col - 1; i < n && j >= 0; ++i, --j) {
              if(board[i][j] == 'Q') return false;
         }
         return true;
