821f92b80be90194e8f6184bbe428c0c650b0ebb
Manish Sencha <39882835+manishsencha@users.noreply.github.com>
2022-08-25 10:21:15 +0530

    Time: 21 ms (65.89%), Space: 8.7 MB (89.81%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> r(26, 0), m(26, 0);
+        for(auto i : ransomNote) {
+            r[i - 'a']++;
+        }
+        for(auto i : magazine) {
+            m[i - 'a']++;
+        }
+        for(int i = 0; i < 26; ++i) {
+            if(r[i] > m[i]) return false;
+        }
+        return true;
+    }
+};
