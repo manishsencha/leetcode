132201f7cd99daeca5e2171f7136c7612f5e14fb
Manish Sencha <39882835+manishsencha@users.noreply.github.com>
2022-08-28 23:40:24 +0530

    Time: 0 ms (100.00%), Space: 6 MB (63.73%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -3,26 +3,13 @@ public:
     bool res = false;
     bool reorderedPowerOf2(int n) {
         string s = to_string(n);
-        vector<int> val;
-        for(int i = 1; i <= 1e9; i *= 2) val.push_back(i);
-        int l = s.length();
-        f(s, 0, l, val);
-        return res;
-    }
-    void f(string s, int ind, int n, vector<int> &v) {
-        if(ind == n) {
-            if(s[0] != '0') {
-                int a = stoi(s);
-                for(auto i : v) {
-                    if(i == a) res |= true;
-                }
-            }
-            return;
+        sort(s.begin(), s.end());
+        // 9 * log2(10) ~ 30
+        for(int i = 0; i < 30; ++i) {
+            string t = to_string(1 << i);
+            sort(t.begin(), t.end());
+            if(t == s) return true;
         }
-        for(int i = ind; i < n; ++i) {
-            swap(s[i], s[ind]);
-            f(s, ind + 1, n, v);
-            swap(s[i], s[ind]);
-        }
+        return false;
     }
 };
