8701b71f1d4d0173d1cc6fcf0430eafe9e3787a1
Manish Sencha <39882835+manishsencha@users.noreply.github.com>
2022-09-12 02:26:45 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.45%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -3,12 +3,12 @@ public:
     int climbStairs(int n) {
         if(n == 1) return 1;
         else if(n == 2) return 2;
-        vector<int> v(n);
-        v[0] = 1;
-        v[1] = 2;
+        int a = 1, b = 2;
         for(int i = 2; i < n; ++i) {
-            v[i] = v[i - 2] + v[i - 1];
+            int t = a + b;
+            a = b;
+            b = t;
         }
-        return v[n - 1];
+        return b;
     }
 };
